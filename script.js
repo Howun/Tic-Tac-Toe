298500d050654847605dbfb6b88a47dbd86e708e
Howun Lam <howunlam96@gmail.com>
2021-11-15 17:01:48 +0000

    Just sudo comments on the design and functions

---
+++ script.js
@@ -0,0 +1,13 @@
+//Js concepts and functions to be added:
+
+// Add names to player 1 and player 2
+// First make it so when a grid is clicked it starts with an X and places it in the grid.
+// Make it so when One is pressed it switches to place the alternate symbol.
+// Make it so you are not allowed to overlap a grid that has a symbol placed down.
+// Add set of rules so once a row of three of the same symbol has been achieved the player who has placed it down wins.
+// Add reset game button
+// When reset button pressed, animate a reset to flip all squares to blank
+
+// Additional things that could be added in:
+// - A tally for how many wins each player has.
+// - Reset button specifically for the counter and tally
