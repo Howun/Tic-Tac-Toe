54be35df94b89a10684e789c2707616cfba27c07
Howun Lam <howunlam96@gmail.com>
2021-11-21 01:16:19 +0000

    When game ends, you can no longer input another move

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 //Js concepts and functions to be added:
-
 // Add names to player 1 and player 2
-// First make it so when a grid is clicked it starts with an X and places it in the grid.
-// Make it so when One is pressed it switches to place the alternate symbol.
 // Make it so you are not allowed to overlap a grid that has a symbol placed down.
-// Add set of rules so once a row of three of the same symbol has been achieved the player who has placed it down wins.
-// Add reset game button
 // When reset button pressed, animate a reset to flip all squares to blank
-
 // Additional things that could be added in:
 // - A tally for how many wins each player has.
 // - Reset button specifically for the counter and tally
 
 let allTiles = document.querySelectorAll(".tiles");
-const gameContent = document.querySelector(".gamearea__inner");
 const gameStartReset = document.querySelector(".gameStartReset-btn");
 let playerTurn = document.querySelector(".playerTurn");
 let winningMessage = document.querySelector(".winMessage");
 
 let currentPlayer = "X";
 let previousPlayer = "";
-const winTxt = () =>  `Player ${previousPlayer} wins!`;
-const drawTxt = () =>  `It's a draw!`;
+const winTxt = () => `Player ${previousPlayer} wins!`;
+const drawTxt = () => `It's a draw!`;
 const playerTurnTxt = () => `It's ${currentPlayer}'s turn`;
+
+function gameOver() {
+	currentPlayer = "";
+	playerTurn.innerHTML = "";
+	}
 
 playerTurn.innerHTML = playerTurnTxt();
 
-function winningTxtUpdate() {winningMessage.innerHTML = winTxt()};
-function drawTxtUpdate() {winningMessage.innerHTML = drawTxt()};
+function winningTxtUpdate() {
+	winningMessage.innerHTML = winTxt()
+};
+
+function drawTxtUpdate() {
+	winningMessage.innerHTML = drawTxt()
+};
 
 
 //win combos
@@ -42,34 +45,35 @@ function winningSolutions() {
 		tile8 = document.querySelector(".tile8"),
 		tile9 = document.querySelector(".tile9");
 	if (tile1.innerHTML !== "" && tile1.innerHTML === tile2.innerHTML && tile1.innerHTML === tile3.innerHTML)
-	winningTxtUpdate();
+		(winningTxtUpdate(), gameOver());
 	else if (tile4.innerHTML !== "" && tile4.innerHTML === tile5.innerHTML && tile4.innerHTML === tile6.innerHTML)
-	winningTxtUpdate();
+	(winningTxtUpdate(), gameOver());
 	else if (tile7.innerHTML !== "" && tile7.innerHTML === tile8.innerHTML && tile7.innerHTML === tile9.innerHTML)
-	winningTxtUpdate();
+	(winningTxtUpdate(), gameOver());
 	else if (tile1.innerHTML !== "" && tile1.innerHTML === tile4.innerHTML && tile1.innerHTML === tile7.innerHTML)
-	winningTxtUpdate();
+	(winningTxtUpdate(), gameOver());
 	else if (tile2.innerHTML !== "" && tile2.innerHTML === tile5.innerHTML && tile2.innerHTML === tile8.innerHTML)
-	winningTxtUpdate();
+	(winningTxtUpdate(), gameOver());
 	else if (tile3.innerHTML !== "" && tile3.innerHTML === tile6.innerHTML && tile3.innerHTML === tile9.innerHTML)
-	winningTxtUpdate();
+	(winningTxtUpdate(), gameOver());
 	else if (tile1.innerHTML !== "" && tile1.innerHTML === tile5.innerHTML && tile1.innerHTML === tile9.innerHTML)
-	winningTxtUpdate();
+	(winningTxtUpdate(), gameOver());
 	else if (tile3.innerHTML !== "" && tile3.innerHTML === tile5.innerHTML && tile3.innerHTML === tile7.innerHTML)
-	winningTxtUpdate();
-	else if (tile1.innerHTML !== "" &&
-					tile2.innerHTML !== "" &&
-					tile3.innerHTML !== "" &&
-					tile4.innerHTML !== "" &&
-					tile5.innerHTML !== "" &&
-					tile6.innerHTML !== "" &&
-					tile7.innerHTML !== "" &&
-					tile8.innerHTML !== "" &&
-					tile9.innerHTML !== "")
-	drawTxtUpdate();		
+	(winningTxtUpdate(), gameOver());
+	else if (
+		tile1.innerHTML !== "" &&
+		tile2.innerHTML !== "" &&
+		tile3.innerHTML !== "" &&
+		tile4.innerHTML !== "" &&
+		tile5.innerHTML !== "" &&
+		tile6.innerHTML !== "" &&
+		tile7.innerHTML !== "" &&
+		tile8.innerHTML !== "" &&
+		tile9.innerHTML !== "")
+		(drawTxtUpdate(), gameOver());
 };
 
-// This make
+// When tiles clicked, input the current players tag, set previous player to the player just gone and check if there is a winning grid played
 for (let i = 0; i < allTiles.length; i++) {
 	const tile = allTiles[i];
 	tile.addEventListener("click", () => {
@@ -81,7 +85,6 @@ for (let i = 0; i < allTiles.length; i++) {
 				previousPlayer = "X";
 				playerTurn.innerHTML = playerTurnTxt();
 				winningSolutions();
-				console.log(previousPlayer);
 				break;
 			case "O":
 				tile.innerHTML = currentPlayer;
@@ -89,7 +92,6 @@ for (let i = 0; i < allTiles.length; i++) {
 				previousPlayer = "O";
 				playerTurn.innerHTML = playerTurnTxt();
 				winningSolutions();
-				console.log(previousPlayer);
 				break;
 		};
 	});
