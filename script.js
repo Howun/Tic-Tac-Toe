5f0227ddafd9b56d7bddf362a7574f0c8174977d
Howun Lam <howunlam96@gmail.com>
2021-11-21 02:17:55 +0000

    Mostly completed, just some more styling to add

--- script.js
+++ script.js
@@ -13,71 +13,29 @@ let winningMessage = document.querySelector(".winMessage");
 
 let currentPlayer = "X";
 let previousPlayer = "";
+
 const winTxt = () => `Player ${previousPlayer} wins!`;
 const drawTxt = () => `It's a draw!`;
 const playerTurnTxt = () => `It's ${currentPlayer}'s turn`;
 
-function gameOver() {
-	currentPlayer = "";
-	playerTurn.innerHTML = "";
-	}
 
+//updates the player's turn
 playerTurn.innerHTML = playerTurnTxt();
-
+//function to update the winning message
 function winningTxtUpdate() {
 	winningMessage.innerHTML = winTxt()
 };
-
+//function to update the winningMessage to say you've drawn
 function drawTxtUpdate() {
 	winningMessage.innerHTML = drawTxt()
 };
 
 
-//win combos
-function winningSolutions() {
-	const tile1 = document.querySelector(".tile1"),
-		tile2 = document.querySelector(".tile2"),
-		tile3 = document.querySelector(".tile3"),
-		tile4 = document.querySelector(".tile4"),
-		tile5 = document.querySelector(".tile5"),
-		tile6 = document.querySelector(".tile6"),
-		tile7 = document.querySelector(".tile7"),
-		tile8 = document.querySelector(".tile8"),
-		tile9 = document.querySelector(".tile9");
-	if (tile1.innerHTML !== "" && tile1.innerHTML === tile2.innerHTML && tile1.innerHTML === tile3.innerHTML)
-		(winningTxtUpdate(), gameOver());
-	else if (tile4.innerHTML !== "" && tile4.innerHTML === tile5.innerHTML && tile4.innerHTML === tile6.innerHTML)
-	(winningTxtUpdate(), gameOver());
-	else if (tile7.innerHTML !== "" && tile7.innerHTML === tile8.innerHTML && tile7.innerHTML === tile9.innerHTML)
-	(winningTxtUpdate(), gameOver());
-	else if (tile1.innerHTML !== "" && tile1.innerHTML === tile4.innerHTML && tile1.innerHTML === tile7.innerHTML)
-	(winningTxtUpdate(), gameOver());
-	else if (tile2.innerHTML !== "" && tile2.innerHTML === tile5.innerHTML && tile2.innerHTML === tile8.innerHTML)
-	(winningTxtUpdate(), gameOver());
-	else if (tile3.innerHTML !== "" && tile3.innerHTML === tile6.innerHTML && tile3.innerHTML === tile9.innerHTML)
-	(winningTxtUpdate(), gameOver());
-	else if (tile1.innerHTML !== "" && tile1.innerHTML === tile5.innerHTML && tile1.innerHTML === tile9.innerHTML)
-	(winningTxtUpdate(), gameOver());
-	else if (tile3.innerHTML !== "" && tile3.innerHTML === tile5.innerHTML && tile3.innerHTML === tile7.innerHTML)
-	(winningTxtUpdate(), gameOver());
-	else if (
-		tile1.innerHTML !== "" &&
-		tile2.innerHTML !== "" &&
-		tile3.innerHTML !== "" &&
-		tile4.innerHTML !== "" &&
-		tile5.innerHTML !== "" &&
-		tile6.innerHTML !== "" &&
-		tile7.innerHTML !== "" &&
-		tile8.innerHTML !== "" &&
-		tile9.innerHTML !== "")
-		(drawTxtUpdate(), gameOver());
-};
 
 // When tiles clicked, input the current players tag, set previous player to the player just gone and check if there is a winning grid played
 for (let i = 0; i < allTiles.length; i++) {
 	const tile = allTiles[i];
 	tile.addEventListener("click", () => {
-
 		switch (currentPlayer) {
 			case "X":
 				tile.innerHTML = currentPlayer;
@@ -94,6 +52,7 @@ for (let i = 0; i < allTiles.length; i++) {
 				winningSolutions();
 				break;
 		};
+		break;
 	});
 };
 
@@ -106,3 +65,51 @@ gameStartReset.addEventListener("click", () => {
 		winningMessage.innerHTML = "";
 	}
 });
+
+//sets input to blank and remove who's turn it is
+function gameOver() {
+	currentPlayer = "";
+	playerTurn.innerHTML = "";
+}
+
+//win combos
+function winningSolutions() {
+	const tile1 = document.querySelector(".tile1"),
+		tile2 = document.querySelector(".tile2"),
+		tile3 = document.querySelector(".tile3"),
+		tile4 = document.querySelector(".tile4"),
+		tile5 = document.querySelector(".tile5"),
+		tile6 = document.querySelector(".tile6"),
+		tile7 = document.querySelector(".tile7"),
+		tile8 = document.querySelector(".tile8"),
+		tile9 = document.querySelector(".tile9");
+	if (tile1.innerHTML !== "" && tile1.innerHTML === tile2.innerHTML && tile1.innerHTML === tile3.innerHTML)
+		(winningTxtUpdate(), gameOver());
+	else if (tile4.innerHTML !== "" && tile4.innerHTML === tile5.innerHTML && tile4.innerHTML === tile6.innerHTML)
+		(winningTxtUpdate(), gameOver());
+	else if (tile7.innerHTML !== "" && tile7.innerHTML === tile8.innerHTML && tile7.innerHTML === tile9.innerHTML)
+		(winningTxtUpdate(), gameOver());
+	else if (tile1.innerHTML !== "" && tile1.innerHTML === tile4.innerHTML && tile1.innerHTML === tile7.innerHTML)
+		(winningTxtUpdate(), gameOver());
+	else if (tile2.innerHTML !== "" && tile2.innerHTML === tile5.innerHTML && tile2.innerHTML === tile8.innerHTML)
+		(winningTxtUpdate(), gameOver());
+	else if (tile3.innerHTML !== "" && tile3.innerHTML === tile6.innerHTML && tile3.innerHTML === tile9.innerHTML)
+		(winningTxtUpdate(), gameOver());
+	else if (tile1.innerHTML !== "" && tile1.innerHTML === tile5.innerHTML && tile1.innerHTML === tile9.innerHTML)
+		(winningTxtUpdate(), gameOver());
+	else if (tile3.innerHTML !== "" && tile3.innerHTML === tile5.innerHTML && tile3.innerHTML === tile7.innerHTML)
+		(winningTxtUpdate(), gameOver());
+	else if (
+		tile1.innerHTML !== "" &&
+		tile2.innerHTML !== "" &&
+		tile3.innerHTML !== "" &&
+		tile4.innerHTML !== "" &&
+		tile5.innerHTML !== "" &&
+		tile6.innerHTML !== "" &&
+		tile7.innerHTML !== "" &&
+		tile8.innerHTML !== "" &&
+		tile9.innerHTML !== "")
+		(drawTxtUpdate(), gameOver());
+};
+
+// I know it doesn't look clean haha
